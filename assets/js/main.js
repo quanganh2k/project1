09bea7c740193736e7548b21d6d26777ed4c628d
quanganh <quanganh24112000@gmail.com>
2022-03-19 14:48:21 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,84 @@
+function changeImage () {
+    var path = "./assets/imgs/shortkaki2.webp"
+    document.getElementById("1").setAttribute('src',path)
+}
+
+function changeImage2 () {
+    var path = "./assets/imgs/shortkaki1.webp"
+    document.getElementById("1").setAttribute('src',path)
+}
+
+function changeImage3 () {
+    var path = "./assets/imgs/cam.webp"
+    document.getElementById("2").setAttribute('src',path)
+}
+
+function changeImage4 () {
+    var path = "./assets/imgs/nude.webp"
+    document.getElementById("2").setAttribute('src',path)
+}
+
+function changeImage6 () {
+    var path = "./assets/imgs/đen.webp"
+    document.getElementById("3").setAttribute('src',path)
+}
+
+function changeImage7 () {
+    var path = "./assets/imgs/shortdaicheo.webp"
+    document.getElementById("3").setAttribute('src',path)
+}
+
+function changeImage8 () {
+    var path = "./assets/imgs/vaydacam.webp"
+    document.getElementById("4").setAttribute('src',path)
+}
+
+function changeImage9 () {
+    var path = "./assets/imgs/xanhla.webp"
+    document.getElementById("4").setAttribute('src',path)
+}
+
+function changeImage10 () {
+    var path = "./assets/imgs/sheermidi1.webp"
+    document.getElementById("5").setAttribute('src',path)
+}
+
+function changeImage11 () {
+    var path = "./assets/imgs/sheermidi2.webp"
+    document.getElementById("5").setAttribute('src',path)
+}
+
+function changeImage12 () {
+    var path = "./assets/imgs/sheermidi3.webp"
+    document.getElementById("5").setAttribute('src',path)
+}
+
+function changeImage14 () {
+    var path = "./assets/imgs/aothunden.webp"
+    document.getElementById("7").setAttribute('src',path)
+}
+
+function changeImage15 () {
+    var path = "./assets/imgs/aothunxanh.webp"
+    document.getElementById("7").setAttribute('src',path)
+}
+
+function changeImage16 () {
+    var path = "./assets/imgs/damsheer1.webp"
+    document.getElementById("8").setAttribute('src',path)
+}
+
+function changeImage17 () {
+    var path = "./assets/imgs/damsheer2.webp"
+    document.getElementById("8").setAttribute('src',path)
+}
+
+function changeImage20 () {
+    var path = "./assets/imgs/aoblazer1.webp"
+    document.getElementById("11").setAttribute('src',path)
+}
+
+function changeImage21 () {
+    var path = "./assets/imgs/aoblazer2.webp"
+    document.getElementById("11").setAttribute('src',path)
+}
